ebd427c94587e97e6147f9ec618d4db63d3a20e1
LalitKishor <lalitsinghbanna05@gmail.com>
2019-06-03 00:03:27 +0530

    refactor code

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,27 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
+async function getFilteredImage(req: express.Request, res: express.Response) {
+  try {
+    const imgPath = await filterImageFromURL(req.query.image_url);
+    const readFilePromise = require('util').promisify(require('fs').readFile);
+    const filteredpath = await readFilePromise(imgPath);
+    res.header('content-type', 'image/jpeg').send(filteredpath);
+    deleteLocalFiles([imgPath]);
+  } catch (err) {
+    //validate image url using jimp 
+    if (err.code && err.code == 'ERR_BAD_URL')
+      res.status(422).send({
+        error: true,
+        message: 'Cannot Process Specified URL'
+      })
+    else
+      res.status(500).send({
+        error: true,
+        message: 'Internal Server Error'
+      })
+  }
+}
 (async () => {
 
   // Init the Express application
@@ -13,45 +34,8 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image 
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get("/filteredimage", async (req, res) => {
-    try {
-      const imgPath = await filterImageFromURL(req.query.image_url);
-      const readFilePromise = require('util').promisify(require('fs').readFile);
-      const greyImage = await readFilePromise(imgPath);
-      res.header('content-type', 'image/jpeg').send(greyImage);
-      deleteLocalFiles([imgPath]);
-    } catch (err) {
-      console.log(err);
-      if (err.code && err.code == 'ERR_BAD_URL')
-        res.status(422).send({
-          error: true,
-          message: 'Cannot Process Specified URL'
-        })
-      else
-        res.status(500).send({
-          error: true,
-          message: 'Internal Server Error'
-        })
-    }
+  app.get("/filteredimage", (req, res) => {
+    getFilteredImage(req, res)
   });
 
 
